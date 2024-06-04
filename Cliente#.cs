520dd61b84b1c31f9a8a79cf90bba470032c8845
Louise <166768872+louisebr34@users.noreply.github.com>
2024-06-03 22:49:01 -0300

    Add files via upload

---
+++ Cliente#.cs
@@ -0,0 +1,16 @@
+﻿namespace Cliente_
+{
+    public class Class1
+    {
+
+    }
+}
+public class Cliente
+{
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public string Email { get; set; }
+    public string Profissao { get; set; }
+    public string Telefone { get; set; }
+    public Endereco Endereco { get; set; }
+}
