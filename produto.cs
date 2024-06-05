f206758447a3ab0e52f01b71b6f3156ad98b8099
Louise <166768872+louisebr34@users.noreply.github.com>
2024-06-04 21:03:59 -0300

    Add files via upload

---
+++ produto.cs
@@ -0,0 +1,28 @@
+﻿namespace cliente__
+{
+    public class Class1
+    {
+
+    }
+}
+public class Produto
+{
+    public string Nome { get; private set; }
+
+    public Produto(string nome)
+    {
+        Nome = nome;
+    }
+}
+public Produto(string nome)
+{
+    if (string.IsNullOrWhiteSpace(nome))
+    {
+        throw new ArgumentException("O nome do produto deve ser fornecido.");
+    }
+    Nome = nome;
+}
+var meuProduto = new Produto("Nome do Produto");
+
+
+
