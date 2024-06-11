7f2fa7a7c667ab136bfcf0436c71178857956585
Louise <166768872+louisebr34@users.noreply.github.com>
2024-06-11 20:58:40 -0300

    Add files via upload

---
+++ Link.cs
@@ -0,0 +1,46 @@
+﻿namespace Link
+{
+    public class Class1
+    {
+
+    }
+}
+
+using Comex.Modelos;
+
+namespace Comex.Linq;
+
+internal class Linq
+{
+
+    public static void ExibirDados(List<Produto> produtos)
+    {
+        foreach (var produto in produtos)
+        {
+            Console.WriteLine($"{produto.ExibirInformações}");
+            Console.WriteLine($"\nNome: {produto.Nome} \nDescrição: {produto.Descricao}\nPreço: {produto.PrecoUnitario}");
+
+        }
+    }
+
+    public static void ExibirDadosOrdenadosNome(List<Produto> produtos)
+    {
+        var produtosOrdenados = produtos.OrderBy(produto => produto.Nome).Select(produto => produto.Nome).Distinct().ToList();
+        Console.WriteLine("Lista de produtos ordenadas:");
+        foreach (var produto in produtosOrdenados)
+        {
+            Console.WriteLine($"- Nome: {produto}");
+        }
+
+    }
+
+    public static void ExibirDadosOrdenadosPreco(List<Produto> produtos)
+    {
+        var produtosOrdenados = produtos.OrderBy(produto => produto.PrecoUnitario).Select(produto => produto.PrecoUnitario).ToList();
+        Console.WriteLine("Lista de produtos ordenadas pelo Preço:");
+        foreach (var produto in produtosOrdenados)
+        {
+            Console.WriteLine($"- Preço: {produto}");
+        }
+    }
+}
---
+++ livros.cs
@@ -0,0 +1,25 @@
+﻿namespace livros
+{
+    public class Class1
+    {
+
+    }
+}
+namespace Comex.Modelos;
+
+internal class Livro : Produto, IIdetificavel
+{
+    public Livro(string nome) : base(nome)
+    {
+    }
+
+    public string Isbn { get; set; }
+    public int TotalDePaginas { get; set; }
+
+    public string Identificar()
+    {
+        return $"Livro: {Nome}, ISBN: {Isbn}";
+    }
+
+
+}
